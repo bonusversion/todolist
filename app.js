08f2d2bc700ece764157d37f93c675b617beaeac
mybonus <506832550@qq.com>
2021-10-30 13:13:52 +0800

    ?

--- app.js
+++ app.js
@@ -177,11 +177,11 @@ app.get("/about", function(req, res) {
     res.render("about");
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-    port = 3000;
-}
+// let port = process.env.PORT;
+// if (port == null || port == "") {
+//     port = 3000;
+// }
 
-app.listen(port, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log("Sever has started successfully");
 });
